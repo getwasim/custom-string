50541548fca40bcf841838fd515a140c8c09aff0
Burt Beckwith <burt@burtbeckwith.com>
2015-04-16 21:59:25 -0400

    you need the release plugin to release the plugin

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -11,4 +11,10 @@ grails.project.dependency.resolution = {
         grailsCentral()
         mavenCentral()
     }
+
+    plugins {
+        build ':release:3.1.1', ':rest-client-builder:2.1.1', {
+            export = false
+        }
+    }
 }
